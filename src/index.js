daffb3d64fa4ace0cb4d161e019a9208952d045d
alionakaliuzhnaya <alionakaliuzhnaya@icloud.com>
2021-11-17 21:25:40 +0100

    upgrade

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './sass/main.scss';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '24387363-33de24eb2750f1937d9fd7176';
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+};
+
+refs.form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefaukt();
+}
